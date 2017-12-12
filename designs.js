2b768b92be75fe0959c2fe0c220033c82c00c6b8
Nirlan Neckir Zamprogno de Souza <nirlan858@gmail.com>
2017-12-12 01:57:15 -0200

    Add #colorPicker jQuery code and HTML and CSS initial adjustments

--- designs.js
+++ designs.js
@@ -1,8 +1,18 @@
 // Select color input
+let color;
+
+$('#colorPicker').change(function() {
+	color = $(this).val();
+});
+
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function() {
 
+
+});
+
+// When size is submitted by the user, call makeGrid()
 function makeGrid() {
 
 // Your code goes here!
