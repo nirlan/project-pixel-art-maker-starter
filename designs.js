0dfcabac536fdc8b5c17f8e007ca7484d8148d7a
Nirlan Neckir Zamprogno de Souza <nirlan858@gmail.com>
2017-12-13 18:17:41 -0200

    Add #sizePicker jQuery code and set max value on input element

--- designs.js
+++ designs.js
@@ -5,12 +5,18 @@ $('#colorPicker').change(function() {
 	color = $(this).val();
 });
 
+
 // Select size input
+let width;
+let height;
 
-$('#sizePicker').submit(function() {
-
-
+$('#sizePicker').submit(function(event) {
+	event.preventDefault();
+	width = $('#input_width').val();
+	height = $('#input_height').val();
 });
+
+
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
