d1f28eef83cbc336c73ce3d3a91f9704f31c216d
Nirlan Neckir Zamprogno de Souza <nirlan858@gmail.com>
2017-12-16 02:08:29 -0200

    Change color of selected grid square with 'click' event listener, and make square white with 'contextmenu' event listener

--- designs.js
+++ designs.js
@@ -7,6 +7,13 @@ $(function() {
 		color = $(this).val();
 	});
 
+	// Getting canvas element and obtaining the rendering context
+	let canvas = $('#canvasGrid').get(0);
+	let ctx = canvas.getContext('2d');
+
+	// Fixed size square that will be
+	// composing the grid (16 pixels square side)
+	const BASE_SIZE = 16;
 
 	// Select size input
 	$('#sizePicker').submit(function(event) {
@@ -16,17 +23,11 @@ $(function() {
 		makeGrid(n, m);
 	});
 
+
 	// Drawing canvas grid. It requires
 	// two parameters 'n' = height input, 'm' = width input
 	function makeGrid(n, m) {
 
-		// Getting canvas element and obtaining the rendering context
-		let canvas = $('#canvasGrid').get(0);
-		let ctx = canvas.getContext('2d');
-
-		// Fixed size square that will be
-		// composing the grid
-		const BASE_SIZE = 16;
 
 		// Size input will be multiplied by 'BASE_SIZE' to get canvas width
 		// and height
@@ -40,10 +41,9 @@ $(function() {
 		// Checking IF canvas height and/or width fits the viewport size
 		// Setting canvas size according to viewport size to better fit the window
 		// if needed
+		if (canvasHeight > viewportHeight*0.6) {  // canvas height greater than 60% vh
 
-		if (canvasHeight > viewportHeight*0.6) {  //canvas height greater than 60% vh
-
-			//need to reset 'n' to an integer multiple of 16px
+			// Need to reset 'n' to an integer multiple of 16px
 			let y = viewportHeight*0.6/BASE_SIZE;
 			n = Math.floor(y);
 			canvasHeight = n*BASE_SIZE;
@@ -52,9 +52,9 @@ $(function() {
 			$('#input_height').val(n);
 		}
 
-		if (canvasWidth > viewportWidth*0.9) {   //canvas width greater than 90% vw
+		if (canvasWidth > viewportWidth*0.9) {   // canvas width greater than 90% vw
 
-			//need to reset 'm' to an integer multiple of 16px
+			// Need to reset 'm' to an integer multiple of 16px
 			let x = viewportWidth*0.9/BASE_SIZE;
 			m = Math.floor(x);
 			canvasWidth = m*BASE_SIZE;
@@ -67,20 +67,70 @@ $(function() {
 		canvas.height = canvasHeight;
 		canvas.width = canvasWidth;
 
-		//Drawing the vertical lines
+		// Drawing the vertical lines
 		for (let i = 0; i <= m; i++) {
 			ctx.beginPath();
+			ctx.lineStyle = 'rgb(10,10,10)';
 			ctx.moveTo(i*BASE_SIZE, 0)
 			ctx.lineTo(i*BASE_SIZE, canvasHeight);
 			ctx.stroke();
 		}
 
-		//Drawing the horizontal lines
+		// Drawing the horizontal lines
 		for (let i = 0; i <= n; i++) {
 			ctx.beginPath();
+			ctx.lineStyle = 'rgb(10,10,10)';
 			ctx.moveTo(0, i*BASE_SIZE)
 			ctx.lineTo(canvasWidth, i*BASE_SIZE);
 			ctx.stroke();
 		}
 	}
+
+	// Change color of the selected grid square
+	$('#canvasGrid').click(function(event) {
+		let offset = $(this).offset(); // get the element offset
+		let x = event.pageX - offset.left; // relative 'x' coordinate of event
+		let y = event.pageY - offset.top; // relative 'y' coordinate of event
+		let xMod = x%BASE_SIZE; // Modulo of 'x' by BASE_SIZE
+		let yMod = y%BASE_SIZE; // Modulo of 'x' by BASE_SIZE
+		let xSqr = x - xMod; // 'x' square coordinate
+		let ySqr = y - yMod; // 'y' square coordinate
+
+		// If the mouse click is inside the square, not over the grid lines
+		if (xMod !== 0 && yMod !== 0) {
+
+			// Draw selected square with selected color
+			ctx.beginPath(); // empty the previous  paths
+			ctx.rect(xSqr, ySqr, BASE_SIZE, BASE_SIZE); // path for the square
+			ctx.fillStyle = color; // fill the square with selected color
+			ctx.lineStyle = 'rgb(10,10,10)';
+			ctx.fill();
+			ctx.stroke();
+		}
+	});
+
+	// Right click makes that square become white, so it is easy correcting
+	// misclicks
+	$('#canvasGrid').contextmenu(function(event) {
+		event.preventDefault();
+		let offset = $(this).offset(); // get the element offset
+		let x = event.pageX - offset.left; // relative 'x' coordinate of event
+		let y = event.pageY - offset.top; // relative 'y' coordinate of event
+		let xMod = x%BASE_SIZE; // Modulo of 'x' by BASE_SIZE
+		let yMod = y%BASE_SIZE; // Modulo of 'x' by BASE_SIZE
+		let xSqr = x - xMod; // 'x' square coordinate
+		let ySqr = y - yMod; // 'y' square coordinate
+
+		// If the mouse click is inside the square, not over the grid lines
+		if (xMod !== 0 && yMod !== 0) {
+
+			// Draw selected square with selected color
+			ctx.beginPath(); // empty the previous  paths
+			ctx.rect(xSqr, ySqr, BASE_SIZE, BASE_SIZE); // path for the square
+			ctx.fillStyle = '#FFF'; // fill the square with selected color
+			ctx.lineStyle = 'rgb(10,10,10)';
+			ctx.fill();
+			ctx.stroke();
+		}
+	});
 });
